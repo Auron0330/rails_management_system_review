0e5c1ef97956e72d49239e17f29644c2b1613603
Auron0330 <auron0330@gmail.com>
2021-08-30 15:13:13 +0900

    8-5まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,30 @@
 class OrdersController < ApplicationController
   def cart
+    @products = Product.all
   end
 
   def new
+    @order = Order.new
+     params[:orders].each do |order|
+      if order[1]["product"].present?
+        order[1].permit("product", "quantity")
+        @order.order_details.new(product_id: order[1]["product"], quantity: order[1]["quantity"])
+      end
+    end
   end
 
   def create
+    @order = Order.new(order_params)
+
+    # テスト用の仮のユーザ
+    user = User.first
+    @order.user_id = user.id
+
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -15,5 +34,12 @@ class OrdersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:number, :occured_at, :payment, :payment_stat, 
+      order_details_attributes: [:id, :discount_rate, :quantity, :order_id, :product_id])
   end
 end
