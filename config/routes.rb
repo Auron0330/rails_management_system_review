f5b03041e1d6afebbb8ccbec020e631ab2376aa0
Auron0330 <auron0330@gmail.com>
2021-08-30 15:03:38 +0900

    8-4まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products                 #⇒（リソースフルな商品ルーティング）
+  resources :orders, only: [:index, :create, :destroy, :show] do     #⇒（従来のnewアクションを削除）
+    get :cart, on: :collection        #⇒（getメソッドに基づく、cartアクションのルートを追加）
+    put :new, on: :collection         #⇒（PUTメソッドに基づく、newアクションのルートを追加）
+  end
 end
